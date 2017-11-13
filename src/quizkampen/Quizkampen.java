51fdf7339775741dbfbfbe15cab7ae3217a40f55
Jozephine <jozephine.gronqvist@outlook.com>
2017-11-13 10:09:28 +0100

    Funkar detta?

---
+++ src/quizkampen/Quizkampen.java
@@ -0,0 +1,17 @@
+
+package quizkampen;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Jozephine
+ */
+public class Quizkampen {
+
+    public static void main(String[] args) {
+        
+        JOptionPane.showMessageDialog(null, "Ser ni mig?");
+    }
+    
+}
